0d01b7f2638dc408246418b7fa885e43baf96061
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2025-11-23 15:07:49 -0300

    feat: installing and configuring react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
 function App() {
   return (
-    <>
-      <h1>VISO-BASS</h1>
-    </>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">Sobre</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<h1>Página Não Encontrada (404)</h1>} />
+      </Routes>
+    </div>
   );
 }
 
