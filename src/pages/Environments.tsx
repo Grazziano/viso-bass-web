66f50ac556fe309aadef7623f1e14d55b02393a9
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2025-12-06 11:14:57 -0300

    feat: add title to environments and friendships pages

--- src/pages/Environments.tsx
+++ src/pages/Environments.tsx
@@ -1,10 +1,14 @@
+import Title from '@/components/common/Title';
 import Layout from '@/components/layouts/Layout';
 
 export default function Environments() {
   return (
     <Layout>
-      <div>
-        <h1>Environments</h1>
+      <div className="space-y-6">
+        <Title
+          title="Ambientes ONA"
+          subtitle="Gerencie os ambientes e suas localizações"
+        />
       </div>
     </Layout>
   );
--- src/pages/Friendships.tsx
+++ src/pages/Friendships.tsx
@@ -1,10 +1,14 @@
+import Title from '@/components/common/Title';
 import Layout from '@/components/layouts/Layout';
 
 export default function Friendships() {
   return (
     <Layout>
-      <div>
-        <h1>Friendships</h1>
+      <div className="space-y-6">
+        <Title
+          title="Relações de Amizade (PageRank)"
+          subtitle="Analise as relações mais relevantes entre objetos"
+        />
       </div>
     </Layout>
   );
