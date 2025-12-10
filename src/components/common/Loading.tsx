6adf82457bfc81acee71abc17d5737dc7fa123ec
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2025-12-10 09:44:54 -0300

    feat: updating loading component

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,11 +1,11 @@
 import Layout from '../layouts/Layout';
-import { Loader2 } from 'lucide-react';
+import { Spinner } from '../ui/spinner';
 
 export default function Loading() {
   return (
     <Layout>
       <div className="flex justify-center py-20">
-        <Loader2 className="w-8 h-8 animate-spin" />
+        <Spinner className="size-8 text-blue-500" />
       </div>
     </Layout>
   );
